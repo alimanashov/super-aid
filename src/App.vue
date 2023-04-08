61289b89a803c304bc62f89f1cc2dc8832d062c4
Ali <alimanashov@gmail.com>
2023-04-08 17:01:51 +0600

    Initial commit hotfix

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import NavBar from "@/components/NavBar.vue";
+</script>
+
+<template>
+  <div class="app">
+    <NavBar />
+    <RouterView />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.app {
+  min-width: 100vw;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import {classifyQuestion} from "@/api/question";
+
+async function classifyTest() {
+  const res = await classifyQuestion("A person is unconscious, first aid instructions?");
+  alert(res);
+}
+</script>
+
+<template>
+  <div class="container">
+    <div class="home">
+      <h1>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, aliquid architecto dicta dignissimos ducimus
+        earum eligendi harum ipsa maiores mollitia nemo odit perferendis porro recusandae suscipit unde voluptatum.
+        Doloribus, totam.
+      </h1>
+      <button @click="classifyTest">CLASSIFY TEST</button>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  flex-grow: 1;
+  margin-top: 2rem;
+  .home {
+    padding: 1rem;
+    border-radius: 20px;
+    background: $white;
+  }
+}
+</style>
